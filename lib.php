1fb87d1ef918b5ad1d3552c05f8d64537db650d2
Farhan Karmali <farhan6318@gmail.com>
2020-06-30 10:00:54 +0530

    update

--- lib.php
+++ lib.php
@@ -1,12 +1,12 @@
 <?php
 
 function local_moodlemoot_before_standard_html_head() {
-    global $PAGE;
+    /*global $PAGE;
     if ($PAGE->pagetype == 'course-view-topics') {
         $id = optional_param('id', 0, PARAM_INT);
         if ($id == 43) {
             $PAGE->requires->css(new moodle_url('/local/moodlemoot/sponsors.css'));
         }
-    }
+    }*/
 
 }
